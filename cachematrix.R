42fd6e1c1bc3e90495000b2c357b1f74ea82fd88
Sumathi Kadur <suma.kadur@gmail.com>
2017-04-30 12:25:53 +0530

    R programming week 3- cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,56 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function provides functions to
+## - set the cache for the matrix
+## - get the cached matriz
+## - set the inverse of the matrix
+## - get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  ## set the   matrix.  x and m are in the parent scope 
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## get the matrix 
+  get <- function() x
+  
+  ## Set the inverse of the matrix in cache
+  setinverse <- function(inverse) m <<- inverse
+  
+  ## get the cached inverse 
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cachSolve creates the inverse of the matrix x in cache using the funtions in makeCacheMatrix.
+## if the data already exists, it returns the same
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## check if the inverse of the matrix already exists
+  
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting already cached matrix")
+    return(m)
+  }
+  
+  ## get the matrix
+  data <- x$get()
+  
+  ## create the inverse matrix
+  m <- solve(data, ...)
+  
+  ## set the cache with inverse data
+  x$setinverse(m)
+  m
 }
